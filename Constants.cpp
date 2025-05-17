9779700b510f0d05f81ab604cebfd52d35b6514c
datnguyen <datnguyen3142@gmail.com>
2025-05-17 09:29:11 +0700

    update

--- Constants.cpp
+++ Constants.cpp
@@ -6,5 +6,5 @@ int gameSpeed = 4;
 bool playerDead = false;
 
 const float GRAVITY = 0.45f;
-const float JUMP_VELOCITY = -15.8f;  
+const float JUMP_VELOCITY = -16.0f;  
 const int GROUND_Y = 300;            
